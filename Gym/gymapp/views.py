7e823d6dd584d5927d04e9693948b9ff0ee4b8d4
Sachin-Khadka <sachinkhadka298@gmail.com>
2020-01-11 22:25:31 +0545

    search function  added

--- Gym/gymapp/views.py
+++ Gym/gymapp/views.py
@@ -11,3 +11,23 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib import auth
 from django.core.exceptions import ObjectDoesNotExist
+
+
+# View Function for Search
+# Search Funcanality
+
+def search_function_hai(request):
+    if request.method =='GET':
+        freddie = request.GET['hacker']
+        if freddie:
+            matchstore = Gym.objects.filter(Q(workoutname__icontains=freddie))                             
+            if matchstore:                
+                return render(request,'gymapp/upload_form.htm', {'sac':matchstore})
+            else:
+                messages.error(request, "The word, You type did  not Exist")
+                # return HttpResponse("The word, You type was not Exist")
+        else:
+            return HttpResponseRedict('gymapp/premium.htm')  
+    return render(request, 'gymapp/premium.htm')         
+
+
